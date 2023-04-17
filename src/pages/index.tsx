86484d914ce384c702ce65e0b5f8401d7be6a3d5
Milk-Maven <tylerf66@gmail.com>
2023-04-17 12:50:59 -0500

    Added win check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,67 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+const DEFAULT_BOARD_LAYOUT = {
+  0: "",
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+  6: "",
+  7: "",
+  8: "",
+};
 const Home: NextPage = () => {
-  const [currentPlayer, setCurrentPlayer] = useState("player X");
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [tiles, setTiles] = useState(DEFAULT_BOARD_LAYOUT);
+  useEffect(() => {
+    // step 1
+    const playerToStart = Math.random() > 0.5 ? "X" : "O";
+    setCurrentPlayer(playerToStart);
+    //
+  }, []);
+
+  // Added another useEffect however unlike the useEffect above this useEffect only fires off when the tiles are updated with a new value
+  // Explain how this useEffect knows to fire off only when the tiles are updated
+  useEffect(() => {
+    console.log("checking for win");
+    checkForWin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tiles]);
+  const checkForWin = () => {
+    // win combos:
+    // horizontal
+    // 0,1,2
+    // 3,4,5
+    // 6,7,8
+    // vertical
+    // 0,3,6
+    // 1,4,7
+    // 2,5,8
+    // diagonal
+    // 0,4,8
+    // 2,4,6
+    // mitch below you'll find an example to check for a horizontal pair. Pleas provide solutions for the other checks below
+    // and remember if you catch yourself repeating code feel free to create a different function to refactor it into another function like we did with onTileClick
+    if (tiles[0] === "X" && tiles[1] === "X" && tiles[2] === "X") {
+      alert("X wins");
+    }
+
+    if (tiles[0] === "O" && tiles[1] === "O" && tiles[2] === "O") {
+      alert("O wins");
+    }
+  };
+
+  const onTileClick = (index: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8) => {
+    if (tiles[index] !== "") {
+      console.log("peepee");
+      return;
+    }
+    setTiles({ ...tiles, [index]: currentPlayer });
+    const nextPlayer = currentPlayer === "X" ? "O" : "X";
+    setCurrentPlayer(nextPlayer);
+  };
   return (
     <>
       <Head>
@@ -14,21 +72,91 @@ const Home: NextPage = () => {
       <main className=" p-10">
         <div className="text-center">its {currentPlayer}s turn</div>
         <div className="flex justify-center">
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>
-          <div className="min-h-[200px] min-w-[200px] border-2 border-black"></div>
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(0);
+            }}
+          >
+            {tiles[0]}
+          </div>
+          <div
+            className="min-h-[200px] min-w-[200px] border-2 border-black"
+            onClick={() => {
+              onTileClick(1);
+            }}
+          >
+            {tiles[1]}
+          </div>
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(2);
+            }}
+          >
+            {tiles[2]}
+          </div>
         </div>
-
         <div className="flex justify-center">
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(3);
+            }}
+          >
+            {tiles[3]}
+          </div>
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(4);
+            }}
+          >
+            {tiles[4]}
+          </div>
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(5);
+            }}
+          >
+            {tiles[5]}
+          </div>
         </div>
-
         <div className="flex justify-center">
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>{" "}
-          <div className="min-h-[200px] min-w-[200px]  border-2 border-black"></div>
-          <div className="min-w-[200px]  border-2 border-black"></div>
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(6);
+            }}
+          >
+            {tiles[6]}
+          </div>{" "}
+          <div
+            className="min-h-[200px] min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(7);
+            }}
+          >
+            {tiles[7]}
+          </div>
+          <div
+            className="min-w-[200px]  border-2 border-black"
+            onClick={() => {
+              onTileClick(8);
+            }}
+          >
+            {tiles[8]}
+          </div>
+        </div>
+        <div>
+          1. do rng in the start to determine who goes first
+          <br />
+          2. when player 1 clicks the tile it should change to their marker
+          <br />
+          3. check for a win
+          <br />
+          4. let players know who won
         </div>
       </main>
     </>
